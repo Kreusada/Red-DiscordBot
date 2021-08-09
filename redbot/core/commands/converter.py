33eb2510f128e951b9ccdaed0201d297481bd91e
Kreusada <67752638+Kreusada@users.noreply.github.com>
2021-08-09 15:34:06 +0100

    remove ugly quotes

--- redbot/core/commands/converter.py
+++ redbot/core/commands/converter.py
@@ -407,8 +407,7 @@ if not TYPE_CHECKING:
         async def convert(self, ctx, arg):
             if arg in self.valid_names:
                 return arg
-            quoted_args = [f"'{name}'" for name in self.valid_names]
-            raise BadArgument(_("Expected {}.").format(humanize_list(quoted_args, style="or")))
+            raise BadArgument(_("Expected {}.").format(humanize_list(self.valid_names, style="or")))
 
         def __class_getitem__(cls, k):
             if not k:
