cb15b174aba691334a54a6daa52a9d45fd729886
Kreusada <67752638+Kreusada@users.noreply.github.com>
2021-08-09 15:30:26 +0100

    Fix formatting

--- redbot/core/commands/converter.py
+++ redbot/core/commands/converter.py
@@ -407,8 +407,8 @@ if not TYPE_CHECKING:
         async def convert(self, ctx, arg):
             if arg in self.valid_names:
                 return arg
-            quoted_args = [f"\"{name}\"" for name in self.valid_names]
-            raise BadArgument(_("Expected {}").format(humanize_list(quoted_args, style="or")))
+            quoted_args = [f"'{name}'" for name in self.valid_names]
+            raise BadArgument(_("Expected {}.").format(humanize_list(quoted_args, style="or")))
 
         def __class_getitem__(cls, k):
             if not k:
