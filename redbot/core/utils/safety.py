326904bbe227d1c5e0c02d6c43722f45ba09cf04
Kreus Amredes <67752638+Kreusada@users.noreply.github.com>
2021-09-11 08:59:31 +0100

    Initial commit

--- redbot/core/utils/safety.py
+++ redbot/core/utils/safety.py
@@ -1,16 +1,37 @@
-import warnings
 import functools
+import types
+import warnings
+
+from typing import Optional
 
 
-def unsafe(f, message=None):
+def unsafe(function: types.FunctionType, message: Optional[str] = None):
     """
     Decorator form for marking a function as unsafe.
 
-    This form may not get used much, but there are a few cases
-    we may want to add something unsafe generally, but safe in specific uses.
-
     The warning can be suppressed in the safe context with warnings.catch_warnings
     This should be used sparingly at most.
+
+    Parameters
+    ----------
+    message : Optional[str]
+        The optional message used to warn that the function is unsafe.
+        Defaults to "<function_name> is unsafe for use" if not provided.
+
+    Examples
+    --------
+    .. testsetup::
+
+        from redbot.core.utils.safety import unsafe
+
+    .. doctest::
+
+        >>> humanize_list(['One', 'Two', 'Three'])
+        'One, Two, and Three'
+        >>> humanize_list(['One'])
+        'One'
+        >>> humanize_list(['omena', 'peruna', 'aplari'], style='or', locale='fi')
+        'omena, peruna tai aplari'
     """
 
     def wrapper(func):
@@ -25,16 +46,24 @@ def unsafe(f, message=None):
     return wrapper
 
 
-def warn_unsafe(f, message=None):
+def warn_unsafe(function: types.FunctionType, message: Optional[str] = None):
     """
-    Function to mark function from dependencies as unsafe for use.
-
-    Warning: There is no check that a function has already been modified.
-    This form should only be used in init, if you want to mark an internal function
-    as unsafe, use the decorator form above.
+    Function form to mark function from dependencies as unsafe for use.
 
     The warning can be suppressed in safe contexts with warnings.catch_warnings
     This should be used sparingly at most.
+
+    .. warning:: There is no check that a function has already been modified.
+        This form should only be used in init, if you want to mark an internal function
+        as unsafe, use the decorator form above.
+
+    Parameters
+    ----------
+    function : types.FunctionType
+        The function to mark as unsafe.
+    message : Optional[str]
+        The optional message used to warn that the function is unsafe.
+        Defaults to "<function_name> is unsafe for use" if not provided.
     """
 
     def wrapper(func):
@@ -46,4 +75,4 @@ def warn_unsafe(f, message=None):
 
         return get_wrapped
 
-    return wrapper(f)
+    return wrapper(function)
