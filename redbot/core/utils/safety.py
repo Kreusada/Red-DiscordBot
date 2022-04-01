c3ad7ea37d253fd00271bc45045e16dc8067870c
Kreusada <67752638+Kreusada@users.noreply.github.com>
2022-04-01 10:32:47 +0100

    Initial commit

--- redbot/core/utils/safety.py
+++ redbot/core/utils/safety.py
@@ -1,49 +1,75 @@
-import warnings
 import functools
+import types
+import warnings
+from typing import Optional
 
 
-def unsafe(f, message=None):
+def _get_wrapper(message):
+    def inner(f):
+        @functools.wraps(func)
+        def get_wrapped(*args, **kwargs):
+            actual_message = message or f"{func.__name__} is unsafe for use"
+            warnings.warn(actual_message, stacklevel=3, category=RuntimeWarning)
+            return func(*args, **kwargs)
+
+        return get_wrapped
+    return inner
+
+
+def unsafe(message: Optional[str] = None):
     """
     Decorator form for marking a function as unsafe.
 
-    This form may not get used much, but there are a few cases
-    we may want to add something unsafe generally, but safe in specific uses.
+    Parameters
+    ----------
+    message : Optional[str]
+        The message included in the warning. Defaults to
+        "[function_name] is unsafe for use"
+        
+    Returns
+    -------
+    types.FunctionType
+        The wrapper for the decorator.
 
-    The warning can be suppressed in the safe context with warnings.catch_warnings
-    This should be used sparingly at most.
+    Example
+    -------
+
+    .. code-block:: python
+
+        @unsafe(message="This function isn't safe, use function2 instead")
+        def function(*args, **kwargs) -> str:
+            ...
     """
 
-    def wrapper(func):
-        @functools.wraps(func)
-        def get_wrapped(*args, **kwargs):
-            actual_message = message or f"{func.__name__} is unsafe for use"
-            warnings.warn(actual_message, stacklevel=3, category=RuntimeWarning)
-            return func(*args, **kwargs)
-
-        return get_wrapped
-
-    return wrapper
+    return _get_wrapper(message)
 
 
-def warn_unsafe(f, message=None):
+def warn_unsafe(f: types.FunctionType, message: Optional[str] = None):
     """
     Function to mark function from dependencies as unsafe for use.
 
-    Warning: There is no check that a function has already been modified.
-    This form should only be used in init, if you want to mark an internal function
-    as unsafe, use the decorator form above.
+    .. warning:: 
+        There is no check that a function has already been modified.
+        This form should only be used in init, if you want to mark an internal function
+        as unsafe, use the decorator form above.
 
-    The warning can be suppressed in safe contexts with warnings.catch_warnings
-    This should be used sparingly at most.
+    Parameters
+    ----------
+    f : types.FunctionType
+        The function to warn as unsafe for use.
+    message : Optional[str]
+        The message included in the warning. Defaults to
+        "[function_name] is unsafe for use"
+
+    Example
+    -------
+
+    .. code-block:: python
+
+        def function(*args, **kwargs) -> str:
+            return "This function isn't safe"
+        warn_unsafe(function, "This function isn't safe, use function2 instead")
     """
 
-    def wrapper(func):
-        @functools.wraps(func)
-        def get_wrapped(*args, **kwargs):
-            actual_message = message or f"{func.__name__} is unsafe for use"
-            warnings.warn(actual_message, stacklevel=3, category=RuntimeWarning)
-            return func(*args, **kwargs)
-
-        return get_wrapped
-
+    wrapper = _get_wrapper(message)
     return wrapper(f)
